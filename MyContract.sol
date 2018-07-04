adcf1dbe0e0b26bd6a7c37c8f10991ffb6a779d6
Hallan Medeiros <hmhallan@gmail.com>
2018-07-04 11:46:40 -0300

    contrato exemplo

---
+++ MyContract.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.18;
+
+contract MyContract {
+    address creator;
+    uint256 myNumber;
+
+    function construtor() public {
+        creator = msg.sender;
+        myNumber = 3;
+    }
+
+    function getCreator() public constant returns(address) {
+        return creator;
+    }
+
+    function getMyNumber() public constant returns(uint256) {
+        return myNumber;
+    }
+
+    function setMyNumber(uint256 myNewNumber) public {
+        myNumber = myNewNumber;
+    }
+
+    function kill() public {
+        if(msg.sender == creator) {
+            selfdestruct(creator);
+        }
+    }
+}
